3fc08da63786f95d2a8565f0035019aeeff01a31
NL628 <45132769+NL628@users.noreply.github.com>
2018-11-22 15:12:42 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,6 +1,14 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+#define FOR(i, N) for (int i = 0; i < N; i++)
+#define PQ priority_queue
+#define IP pair<int, int>
+#define VI vector<int>
+#define LL long long
+#define PB push_back
+#define MP make_pair
+
 ofstream fout (".out");
 ifstream fin (".in");
 
