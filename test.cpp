a3cef379432e0ca45cd5eb30d03ce65bc07e451a
NL628 <45132769+NL628@users.noreply.github.com>
2018-11-22 15:25:00 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -2,17 +2,22 @@
 using namespace std;
 
 #define FOR(i, N) for (int i = 0; i < N; i++)
+#define FOR(i, a1, a2) for (int i = a1; i < a2; i++)
+#define FORIT(it, A) for (auto it = A.begin(); it != A.end(); it++)
+#define PRINTARR(A, N) FOR(i, N) cout << A[i] << ' '; cout << '\n';
+#define CINARR(cin, A, N) FOR(i, N) cin >> A[i];
 #define PQ priority_queue
 #define IP pair<int, int>
 #define VI vector<int>
 #define LL long long
 #define PB push_back
 #define MP make_pair
+#define INF 1 << 30
+#define LINF 1LL << 60
 
 ofstream fout (".out");
 ifstream fin (".in");
 
 int main() {
-  cout << "Hello, World!" << endl;
-  return 0;
+  
 }
