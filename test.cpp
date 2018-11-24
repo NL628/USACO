b0e8eb54fe9ba765208675f4a5732ba88b6ad58f
Nevin Liang <45132769+NL628@users.noreply.github.com>
2018-11-23 21:16:52 -0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -1,19 +1,30 @@
+// COMPETITION: USACO
+// URL: 
+// DATE: 
+// STATUS: Unfinished
+
 #include <bits/stdc++.h>
 using namespace std;
 
-#define FOR(i, N) for (int i = 0; i < N; i++)
-#define FOR(i, a1, a2) for (int i = a1; i < a2; i++)
-#define FORIT(it, A) for (auto it = A.begin(); it != A.end(); it++)
-#define PRINTARR(A, N) FOR(i, N) cout << A[i] << ' '; cout << '\n';
-#define CINARR(cin, A, N) FOR(i, N) cin >> A[i];
-#define PQ priority_queue
-#define IP pair<int, int>
-#define VI vector<int>
-#define LL long long
-#define PB push_back
-#define MP make_pair
-#define INF 1 << 30
-#define LINF 1LL << 60
+typedef pair<double, double> pd;
+typedef pair<int, int> pi;
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<pi> vpi;
+
+#define min3(a, b, c) min(a, min(b, c))
+#define max3(a, b, c) max(a, max(b, c))
+#define sz(x) (int)(x).size()
+#define mp make_pair
+#define pb push_back
+#define f first
+#define s second
+
+const double PI = 4 * atan(1);
+const int MOD = 1000000007;
+const ll LINF = 1ll << 60;
+const int INF = 1 << 30;
 
 ofstream fout (".out");
 ifstream fin (".in");
